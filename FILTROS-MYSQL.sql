9e552d3072282e2000afbfe6c35b28ea07d87e63
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-05-16 20:48:19 -0500

    filtros por fechas

--- FILTROS-MYSQL.sql
+++ FILTROS-MYSQL.sql
@@ -22,3 +22,24 @@ select * from tbProducto where ENVASE = 'Botella de Vidrio';
 update tbProducto set SABOR = 'Citrico' where SABOR = 'limon';
 select * from tbProducto where SABOR = 'Limon';
 select * from tbProducto where SABOR = 'Citrico';
+
+-- FILTROS POR CONDICIONES
+SELECT * FROM tbcliente where EDAD > 27;
+SELECT * FROM tbcliente where EDAD <= 27;
+SELECT * FROM tbcliente where EDAD <> 25;
+SELECT * FROM tbcliente where EDAD > 'Erica Carvajo';
+
+SELECT * FROM tbProducto where PRECIO_LISTA = 28.51;
+
+SELECT * FROM tbProducto WHERE PRECIO_LISTA BETWEEN 12 AND 29;
+
+-- FILTROS POR FORMATO_FECHA
+SELECT * FROM tbcliente WHERE FECHA_NACIMIENTO < '1995-01-13';
+SELECT * FROM tbcliente WHERE year(FECHA_NACIMIENTO) = 1995;
+SELECT * FROM tbcliente WHERE DAY(FECHA_NACIMIENTO) = 20;
+
+SELECT * FROM tbProducto WHERE PRECIO_LISTA BETWEEN 28 AND 30;
+SELECT * FROM tbProducto WHERE PRECIO_LISTA >= 12 AND PRECIO_LISTA <= 30;
+SELECT * FROM tbProducto WHERE ENVASE = 'Lata' OR ENVASE = 'Botella PET';
+
+SELECT * FROM tbProducto WHERE (PRECIO_LISTA >= 12 AND PRECIO_LISTA <= 30) OR (ENVASE = 'Lata' OR ENVASE = 'Botella PET');
