e7d40dbd0883d2b605c35470aa7685263e913128
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-05-16 20:29:27 -0500

    MySql basic

---
+++ MYSQL-BASIC.sql
@@ -0,0 +1,18 @@
+use Jugos;
+
+-- CREACION DE UNA TABLA EN UNA BASE DE DATOS-- 
+create table TBClientes3(
+DNI varchar(20),
+NOMBRE varchar(150),
+DIRECCION1 varchar(150),
+DIRECTCION2 varchar(150),
+BARRIO varchar(50),
+CIUDAD varchar(50),
+ESTADO varchar(50),
+CP varchar(10),
+EDAD smallint,
+SEXO varchar(1),
+LIMITE_CREDITO float,
+VOLUMEN_COMPRA float,
+PRIMERA_COMPRA bit(1));
+
