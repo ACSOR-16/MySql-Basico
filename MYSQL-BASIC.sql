1c3b9b5c30dbcded8eda1e8e5d20f6d570d67f01
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-05-16 20:35:04 -0500

    comandos basicos de mysql

--- MYSQL-BASIC.sql
+++ MYSQL-BASIC.sql
@@ -1,6 +1,6 @@
 use Jugos;
 
--- CREACION DE UNA TABLA EN UNA BASE DE DATOS-- 
+-- CREACION DE UNA TABLA EN UNA BASE DE DATOS
 create table TBClientes3(
 DNI varchar(20),
 NOMBRE varchar(150),
@@ -16,3 +16,60 @@ LIMITE_CREDITO float,
 VOLUMEN_COMPRA float,
 PRIMERA_COMPRA bit(1));
 
+-- INSERTAR PRODUCTOS EN UNA TABLA 
+insert into Productos(
+PRODUCTO, NOMBRE, ENVASE, VOLUMEN, SABOR, PRECIO) values (
+'773912', 'clean', 'botella pet', '1 litro', 'naranja', 8.01);
+
+SELECT * from Productos;
+
+-- INSERTANDO MAS REGISTROS EN LA TABLA--
+insert into Productos(
+PRODUCTO, NOMBRE, ENVASE, VOLUMEN, SABOR, PRECIO) values (
+'838819', 'clean', 'botella pet', '1,5 litros', 'naranja', 12.01);
+
+insert into Productos(
+PRODUCTO, NOMBRE, ENVASE, VOLUMEN, SABOR, PRECIO) values (
+'1037797', 'clean', 'botella pet', '2 litro', 'naranja', 16.01);
+
+insert into Productos(
+PRODUCTO, NOMBRE, ENVASE, VOLUMEN, SABOR, PRECIO) values (
+'812829', 'clean', 'lata', '350 ml', 'naranja', 2.81);
+
+INSERT INTO Productos(
+PRODUCTO, NOMBRE, ENVASE, VOLUMEN, SABOR, PRECIO) VALUES (
+'695594', 'Festival de Sabores', 'Botella PET',
+'1.5 Litros', 'Asaí', 18.51);
+
+INSERT INTO Productos(
+PRODUCTO, NOMBRE, ENVASE, VOLUMEN, SABOR, PRECIO) VALUES (
+'1041119', 'Línea Citrus', 'Botella de Vidrio',
+'700 ml', 'Lima', 4.90);
+
+select * from Productos;
+
+-- ACTUALIZANDO REGISTROS EN UNA TABLA
+
+update Productos set NOMBRE = 'Clean', ENVASE = 'Botella PET', VOLUMEN = '1 Litro', SABOR = 'Naranja'
+where PRODUCTO = '773912';
+
+update Productos set NOMBRE = 'Clean', ENVASE = 'Botella PET', VOLUMEN = '1,5 Litros', SABOR = 'Naranja'
+where PRODUCTO = '838819';
+
+update Productos set NOMBRE = 'Clean', ENVASE = 'Botella PET', VOLUMEN = '2 Litros', SABOR = 'Naranja'
+where PRODUCTO = '1037797';
+
+update Productos set NOMBRE = 'Clean', ENVASE = 'Lata', SABOR = 'Naranja'
+where PRODUCTO = '812829';
+
+update Productos set PRECIO = 28.51
+where PRODUCTO = '695594';
+
+update Productos set NOMBRE = 'Festival de Sabores', SABOR = 'Lima/limón'
+where PRODUCTO = '1041119';
+
+select * FROM Productos;
+
+-- ELIMINANDO REGISTROS DE LA TABLA
+
+delete from Productos where PRODUCTO = '773912';
